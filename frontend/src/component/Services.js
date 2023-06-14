fc52bb3728f9d708bd934e7033e8a50ef5637773
Anuran12 <anuranarts@gmail.com>
2023-06-14 15:57:55 +0530

    services update

--- frontend/src/component/Services.js
+++ frontend/src/component/Services.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Service1 from '../img/services/service1.png'
+import Service2 from '../img/services/service2.png'
+import Service3 from '../img/services/service3.png'
+import Service4 from '../img/services/service4.png'
+import '../styles/Services.css'
 
 function Services() {
   return (
@@ -8,9 +13,30 @@ function Services() {
             <div className='service'>
                 <div>
                     <h1>CONNECT WITH A LISTENER</h1>
-                    <button></button>
+                    <button>Book Now</button>
                 </div>
-                <img />
+                <img src={Service1}/>
+            </div>
+            <div className='service'>
+                <div>
+                    <h1>LISTENING CIRCLE</h1>
+                    <button>Book Now</button>
+                </div>
+                <img src={Service2}/>
+            </div>
+            <div className='service'>
+                <div>
+                    <h1>LISTENING WORKSHOP</h1>
+                    <button>Book Now</button>
+                </div>
+                <img src={Service3}/>
+            </div>
+            <div className='service'>
+                <div>
+                    <h1>COUNSELING</h1>
+                    <button>Book Now</button>
+                </div>
+                <img src={Service4}/>
             </div>
         </div>
     </div>
