a942e089dfd7fa34af529f71e28ac3844fd76304
Anuran12 <anuranarts@gmail.com>
2023-06-28 02:01:12 +0530

    nav responsive

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Navbar.css'
 import Logo from '../img/logo.png'
 import {Link} from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
     <header class="nav">
         <img
@@ -12,15 +14,36 @@ function Navbar() {
           class="logo-top"
           onclick="window.location = '/';"
         />
-        <nav class="nav-list">
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
+            <nav class="nav-list">
           <Link to='/' class="nav-link scale">Home</Link>
           <Link to='/About' class="nav-link scale">About</Link>
           <Link to='/Services' class="nav-link scale">Service</Link>
           <Link to='/Blogs' class="nav-link scale">Blogs</Link>
           <Link to='/Collaboration' class="nav-link scale">Collaboration</Link>
           <Link to='/Listener' class="nav-link scale">Become A Listener</Link>
+          <div className='nav_btns_mobile'>
+        <a
+          id="nav-btn1"
+          class="nav-btn"
+          >Volunteer
+          </a>
+        <a
+          id="nav-btn2"
+          class="nav-btn"
+          >Donate
+          </a>
+        </div>
+          
 
         </nav>
+
+
+
+        </div>
         <div className='nav_btns'>
         <a
           id="nav-btn1"
@@ -33,6 +56,12 @@ function Navbar() {
           >Donate
           </a>
         </div>
+        <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        
         
         {/* <button class="right" id="collapse-btn" onclick="showMenu(event)">
           <span class="menu-bar"></span>
